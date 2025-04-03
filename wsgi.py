4c9e5087ec6da588fd64a5194b29d19e5ac0eb47
Wolfgang Weigl <w.weigl@oth-aw.de>
2025-04-03 11:27:32 +0200

    fix imports

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,8 @@
 # app/wsgi.py
 # pragma: no cover
 
-from app import app, socketio
+from app.app import app
+from app.socket_events import socketio
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000)
