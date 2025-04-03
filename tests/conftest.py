8ab1c8704b4b98cd9f6674402c37901bfdcdbffd
Wolfgang Weigl <w.weigl@oth-aw.de>
2025-04-03 11:29:06 +0200

    fix imports

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,7 @@
 import pytest
-from app import app as flask_app
+from app.app import app as flask_app
 
 @pytest.fixture
 def client():
     flask_app.config['TESTING'] = True
-    with flask_app.test_client() as client:
-        yield client
+    return flask_app.test_client()
